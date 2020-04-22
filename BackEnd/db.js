2d2c8ea980f6c77a5a71a9df032be6a23c8bef3b
hertafranca <hertafranca.tech@gmail.com>
2020-04-22 18:22:31 -0300

    DataBase Js page

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -9,32 +9,29 @@ db.serialize(function () {
     title TEXT,
     category TEXT,
     description TEXT,
-    link TEXT
-      );
+    link TEXT)
   `);
+  const query = `
+  INSERT INTO ideas (
+    image,
+    title,
+    category,
+    description,
+    link
+  ) VALUES (?,?,?,?,?);
+  `;
+  const values = [
+    "https://as2.ftcdn.net/jpg/00/44/17/11/500_F_44171108_7iRPSwptiKB34mSn1j5DiElTTyFAVHFS.jpg",
+    "Leia para seus filhos nesta quarentena.",
+    "Hora da Leitura Online",
+    "Lendo para uma criança você contribui para ela desenvolver atenção, concentração, memória e raciocínio. Dessa forma, toda criança pode desenvolver seu potencial ao máximo. Quando você lê para uma criança, ela pode buscar o futuro que quiser.",
+    "https://www.euleioparaumacrianca.com.br/",
+  ];
 
-  //const query = `
-  //INSERT INTO ideas(
-  //image,
-  //title,
-  //category,
-  //description,
-  //link
-  //) VALUES (?,?,?,?,?)`;
-
-  //const values = [
-  // "https://as2.ftcdn.net/jpg/00/44/17/11/500_F_44171108_7iRPSwptiKB34mSn1j5DiElTTyFAVHFS.jpg",
-  // "Leia para seus filhos nesta quarentena.",
-  // "Hora da Leitura Online",
-  // "Lendo para uma criança você contribui para ela desenvolver atenção, concentração, memória e raciocínio. Dessa forma, toda criança pode desenvolver seu potencial ao máximo. Quando você lê para uma criança, ela pode buscar o futuro que quiser.",
-  // "https://www.euleioparaumacrianca.com.br/",
-  //];
-
-  //db.run(query, values, function (err) {
-  // if (err) return console.log(err);
-
-  // console.log(this);
-  //});
+  db.run(query, values, function (err) {
+    if (err) return console.log(err);
+    console.log(this);
+  });
 
   //db.run(`DELETE FROM ideas WHERE id = ?`, [1], function (err) {
   // if (err) return console.log(err);
